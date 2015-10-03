56ab8c86cd835ed809bf8b605826096973edfa11
impregnable <ImpregnableFiend@gmail.com>
2015-10-03 04:55:12 +0300

    player routes with sql

--- server/node/server.js
+++ server/node/server.js
@@ -1,12 +1,33 @@
 var express = require("express");
+var mysql = require("mysql");
 var app = express();
 
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "football"
+});
+console.log("Connected to DB");
+
 app.get("/players", function(req, res) {
-  res.send("Players");
+  conn.query("SELECT * FROM players", function(err, rows, fields) {
+    res.json(rows);
+  })
 });
 
 app.get("/players/:player_id", function(req, res) {
-  res.send("Player #" + req.params.player_id);
+  conn.query("SELECT * FROM players WHERE player_id = ?", [req.params["player_id"]], function(err, rows, fields) {
+    if(rows.length == 1) {
+      res.json(rows[0]);
+    }
+    else {
+      res.writeHead(404);
+      res.end();
+    }
+  })
 })
 
-app.listen(9000);
+var port = 9000;
+app.listen(port);
+console.log("Server listening on port " + port);
