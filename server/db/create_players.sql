3819245aa69da360b6cce7cd3ac181e7d21ad248
impregnable <ImpregnableFiend@gmail.com>
2015-10-05 03:36:51 +0300

    Added players_images table

--- server/db/create_players.sql
+++ server/db/create_players.sql
@@ -3,13 +3,13 @@ CREATE DATABASE IF NOT EXISTS football;
 # CREATE TABLE <players> IF NOT EXISTS
 # with necesary columns
 CREATE TABLE IF NOT EXISTS players (
-  player_id INT(8) NOT NULL AUTO_INCREMENT,
+  player_id INT NOT NULL AUTO_INCREMENT,
   player_name CHAR(60) NOT NULL,
   player_lastname CHAR(60) NOT NULL,
   player_birthdate DATE NOT NULL,
   player_nationality CHAR(60) NOT NULL,
   player_club CHAR(60) NOT NULL,
-  player_position ENUM(8) NOT NULL,
+  player_position ENUM NOT NULL,
   player_description TEXT,
   player_characteristics SET,
   player_style TEXT,
@@ -17,5 +17,11 @@ CREATE TABLE IF NOT EXISTS players (
   );
   # player_honours player_goldenball player_goldenshoe  some numbers and a few words
   # player_records # some numbers and a few words
-  # FOREIGN KEY # dunno how to use right
-  # player_images # some images
+CREATE TABLE IF NOT EXISTS players_images (
+  image_id INT NOT NULL AUTO_INCREMENT,
+  image_name CHAR(60) NOT NULL,
+  image_type CHAR(60) NOT NULL,
+  image_size INT NOT NULL,
+  image_content MEDIUMBLOB NOT NULL,
+  PRIMARY KEY (image_id)
+);
